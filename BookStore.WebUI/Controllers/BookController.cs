6d6473badb9219d293f511d38b452ca69c5642db
Amiro <Amiro@DESKTOP-RPRJ7SA>
2022-08-17 15:44:00 +0500

    added some changes

---
+++ BookStore.WebUI/Controllers/BookController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BookStore.Domain.Abstract;
+using BookStore.Domain.Entities;
+
+namespace BookStore.WebUI.Controllers
+{
+    public class BookController : Controller
+    {
+        private IBookRepository repository;
+        public BookController(IBookRepository repo)
+        {
+            repository = repo;
+        }
+    }
+}
--- BookStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ BookStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -4,6 +4,8 @@ using System.Configuration;
 using System.Web.Mvc;
 using Moq;
 using Ninject;
+using BookStore.Domain.Abstract;
+using BookStore.Domain.Entities;
 
 namespace BookStore.WebUI.Infrastructure
 {
@@ -27,9 +29,34 @@ namespace BookStore.WebUI.Infrastructure
             return kernel.GetAll(serviceType);
         }
 
+        
         private void AddBindings()
         {
             // Здесь размещаются привязки
+            Mock<IBookRepository> mock = new Mock<IBookRepository>();
+            mock.Setup(m => m.Books).Returns(new List<Book>
+            {
+                new Book { Name = "Odoblar xazinasi", Price = 160000 },
+                new Book { Name = "Til ofatlari", Price=22900 },
+                new Book { Name = "Baxtiyor oila", Price=65000 },
+                new Book { Name = "Baxtli oila qurishning 5 qoidasi", Price = 1499 },
+
+                new Book { Name = "C# dasturlashlashni o'rganamiz", Price=130000 },
+                new Book { Name = "Dasturlash asoslari", Price=87000 },
+                new Book { Name = "JavaScript va uning fremvorklari", Price = 140000 },
+                new Book { Name = "Html va CSS asoslari", Price=45000 },
+
+                new Book { Name = "Moliy asoslari", Price=210000 },
+                new Book { Name = "Buxgalteriya xisobini yuritish", Price = 70000 },
+                new Book { Name = "Audit", Price=110000 },
+                new Book { Name = "Korxona xisobni yuritish", Price=95000 },
+                new Book { Name = "Iqtisod", Price = 132000 },
+
+                new Book { Name = "Boy ota v kambaxal ota", Price=75000 },
+                new Book { Name = "Million dollorlik xatolar", Price=190000 },
+                new Book { Name = "Muomala sirlari", Price=35000}
+            });
+            kernel.Bind<IBookRepository>().ToConstant(mock.Object);
         }
     }
 }
